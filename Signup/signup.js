b2ee1a3d94e620d488c1b2a270c8493a3ee361cc
pankaj-cmd <prajapatipankaj878@gmail.com>
2022-11-10 01:32:37 +0530

    add day 2

---
+++ Signup/signup.js
@@ -0,0 +1,15 @@
+
+
+// let password_mouse= document.getElementById("password");
+// password_mouse.onmousedown = () => {
+//     document.getElementById("extra_left").style.display = 'none';
+//     console.log("extra")
+// }
+
+
+function myFunction (){
+    document.getElementById("extra_right").style.display = 'block';
+        console.log("el")
+    document.getElementById("extra_left").style.display = 'block';
+        console.log("el")
+}
