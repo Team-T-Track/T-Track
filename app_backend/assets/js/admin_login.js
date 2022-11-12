0b2c78f0664afe647c7aabb51a5c9412c0332c5e
Vibhor Sharma <eminenceindia324@gmail.com>
2022-11-12 19:53:20 +0530

    day-5

--- app_backend/assets/js/admin_login.js
+++ app_backend/assets/js/admin_login.js
@@ -14,19 +14,19 @@ let password= document.getElementById("login-password").value;
     if(username=="kml552"&&password=="password552"){
         alert("welcome Kamal" )
        
-        window.location.replace("./login.html")
+        window.location.href = "./admin_main.html";
     }else if(username=="pankaj_01"&&password=="pankaj_01"){
         alert("welcome Pankaj" )
         window.location.href()
-        window.location.replace("./login.html")
+        window.location.replace("./app_backend/admin_main.html")
     }else if(username=="vibhor"&&password=="passwords"){
         alert("welcome Vibhor" )
         window.location.href()
-        window.location.replace("./login.html")
+        window.location.replace("./app_backend/admin_main.html")
     }else if(username=="aniket"&&password=="aniket07"){
         alert("welcome aniket" )
         window.location.href()
-        window.location.replace("./login.html")
+        window.location.replace("./app_backend/admin_main.html")
     }else{
         alert("wrong details")
     }
