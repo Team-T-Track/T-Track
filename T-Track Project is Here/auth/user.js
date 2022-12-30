7890289c12d3a97a609738b2976552f4ecb9efa8
Vibhor Sharma <eminenceindia324@gmail.com>
2022-12-30 23:10:48 +0530

    Folder Management

--- T-Track Project is Here/auth/user.js
+++
@@ -1,9 +0,0 @@
-import {append} from "./srcipts/append.js";
-let maindiv= document.getElementById("append");
-const getDAta =async()=>{
-    let res = await fetch(`http://localhost:3000/users`);
-    let data = await res.json();
-    console.log(data)
-    append(data,maindiv)
-    }
-    getDAta()
--- T-Track Project is Here/autht/user.js
+++
@@ -1,9 +0,0 @@
-import {append} from "./srcipts/append.js";
-let maindiv= document.getElementById("append");
-const getDAta =async()=>{
-    let res = await fetch(`https://floating-island-82380.herokuapp.com/users`);
-    let data = await res.json();
-    console.log(data)
-    append(data,maindiv)
-    }
-    getDAta()
