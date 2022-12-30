4f4ae7569d0e292cd2f8fac53f2a289b87fe8917
Vibhor Sharma <eminenceindia324@gmail.com>
2022-12-30 22:41:14 +0530

    more stuff deleted

--- T-Track Project is Here/Click here for the Project Code/script/costumers.js
+++
@@ -1,6 +0,0 @@
-import navbar from "../components/navbar.js";
-import { bottom } from "../bottom/bottom.js";
-let nav = document.getElementById("navbar");
-nav.innerHTML = navbar();
-let bot = document.querySelector(".bottom")
-bot.innerHTML = bottom()
--- T-Track Project is Here/Click here for the Project Code/script/industry.js
+++
@@ -1,6 +0,0 @@
-import { bottom } from "../bottom/bottom.js";
-import navbar from "../components/navbar.js";
-let nav = document.getElementById("navbar");
-nav.innerHTML = navbar();
-let bot = document.querySelector(".bottom")
-bot.innerHTML = bottom()
